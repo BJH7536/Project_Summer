986bfa237506c20fc17d92f9f44dc998d2c9667a
BJH <114412598+BJH7536@users.noreply.github.com>
2024-07-16 19:50:01 +0900

    [UPDATE] 플레이어 움직임 개선

--- Assets/Scripts/PlayerControl/Player.cs
+++ Assets/Scripts/PlayerControl/Player.cs
@@ -75,18 +75,25 @@ public class Player : MonoBehaviour
 
     private void TryPickUpObject()
     {
-        Collider[] colliders = Physics.OverlapSphere(raycastOrigin.position, interactDistance);
-        foreach (Collider collider in colliders)
+        // => 기존에 tag를 통해서 물체를 감지하던 코드를 Layer기반으로 수정.
+        int interactableLayer = LayerMask.GetMask("Interactable");
+        
+        // 최대 감지할 수 있는 충돌체 수를 정의 (예: 10)
+        Collider[] colliders = new Collider[10];
+        
+        // 충돌체 감지
+        int numColliders = Physics.OverlapSphereNonAlloc(raycastOrigin.position, interactDistance, colliders, interactableLayer);
+
+        for (int i = 0; i < numColliders; i++)
         {
-            if (collider.CompareTag("Interactable"))
-            {
-                heldObject = collider.gameObject;
-                heldObject.transform.SetParent(holdPosition);
-                heldObject.transform.localPosition = Vector3.zero;
-                heldObject.GetComponent<Rigidbody>().isKinematic = true;
-                Debug.Log("Picked up " + heldObject.name);
-                break;
-            }
+            Collider collider = colliders[i];
+            heldObject = collider.gameObject;
+            heldObject.transform.SetParent(holdPosition);
+            heldObject.transform.localPosition = Vector3.zero;
+            heldObject.GetComponent<Rigidbody>().isKinematic = true;
+            heldObject.GetComponent<Collider>().isTrigger = true;       // 플레이어 캐릭터와 충돌하지 않도록 수정
+            Debug.Log("Picked up " + heldObject.name);
+            break;
         }
     }
 
@@ -95,8 +102,8 @@ public class Player : MonoBehaviour
         Debug.Log("Dropped "+heldObject.name);
         heldObject.transform.SetParent(null);
         heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.GetComponent<Collider>().isTrigger = false;
         heldObject = null;
-       
     }
 
     private void FixedUpdate()
@@ -125,6 +132,8 @@ public class Player : MonoBehaviour
         {
             _player = player;
             _player._animator.SetBool("isRun", false);
+            _player._rigidbody.velocity = Vector3.zero;
+            _player._rigidbody.angularVelocity = Vector3.zero;
         }
 
         public void Execute()
@@ -146,12 +155,17 @@ public class Player : MonoBehaviour
 
         public void Execute()
         {
-            Vector3 movement = new Vector3(_player.inputVector.x, 0, _player.inputVector.y) * _player.speed * Time.fixedDeltaTime;
-            _player._rigidbody.MovePosition(_player._rigidbody.position + movement);
+            Vector3 movement = new Vector3(_player.inputVector.x, 0, _player.inputVector.y).normalized * _player.speed;
 
             if (movement != Vector3.zero)
             {
-                _player.transform.LookAt(_player.transform.position + movement);
+                // 플레이어 캐릭터의 Rigidbody
+                _player._rigidbody.velocity = movement;
+                //_player._rigidbody.MovePosition(_player._rigidbody.position + movement * Time.fixedDeltaTime);
+                
+                //_player.transform.LookAt(_player.transform.position + movement);
+                // 바라보는 방향이 좀 더 부드럽게 변하도록 보간을 추가
+                _player.transform.rotation = Quaternion.Lerp(_player.transform.rotation, Quaternion.LookRotation(movement), 10*Time.deltaTime);
             }
         }
 
